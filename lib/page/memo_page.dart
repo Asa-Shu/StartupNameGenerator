bef3e86d023287f02db9092d1260a14238c7e6d6
Asa-Shu <shuki0523@yahoo.co.jp>
2023-02-21 22:43:31 +0900

    first commit

---
+++ lib/page/memo_page.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+// メモページのウィジェット（動的）
+class MemoPage extends StatefulWidget {
+  // コンストラクタ
+  @override
+  MemoPage({Key? key}) : super(key: key) {}
+
+  @override
+  _memoPageState createState() => _memoPageState();
+}
+
+class _memoPageState extends State<MemoPage> {
+  // 初期化処理
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  // 削除処理
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
+  // ウィジェット生成
+  
+}
